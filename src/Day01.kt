a9611c7b275eacbcfa6064c6f6c61809c27283af
Oleksandr Balan <alex.balan.wewox@gmail.com>
2022-12-01 08:41:49 +0100

    Day 01 ✅

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val calories = input.chunkedBy { it.isEmpty() }
+        .map { list -> list.sumOf { it.toLong() } }
+        .sortedDescending()
+    println("Part1: ${calories.maxOrNull() ?: 0}")
+    println("Part2: ${calories.take(3).sum()}")
+}
+
+private fun <T> List<T>.chunkedBy(separator: (T) -> Boolean): List<List<T>> {
+    val result = mutableListOf<List<T>>()
+    val resultItem = mutableListOf<T>()
+    forEach { item ->
+        if (separator(item)) {
+            result.add(resultItem.toList())
+            resultItem.clear()
+        } else {
+            resultItem.add(item)
+        }
+    }
+    result.add(resultItem.toList())
+    return result
 }

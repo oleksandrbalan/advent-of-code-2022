58a45bd1ae971e91fa3a8106128a5dba2aeebadc
Oleksandr Balan <alex.balan.wewox@gmail.com>
2022-12-05 20:19:29 +0100

    Day 05 ✅

---
+++ src/Day05.kt
@@ -0,0 +1,76 @@
+import java.util.Deque
+import java.util.LinkedList
+
+fun main() {
+    val (movesInput, shipInput) = readInput("Day05")
+        .partition { it.startsWith("move") }
+
+    val stacks = shipInput.first().length / 4 + 1
+    val shipCargo = shipInput
+        .takeWhile { it.trim().startsWith("[") }
+        .map { row -> row.chunked(4) { it[1] } }
+        .reversed()
+
+    val moves = movesInput
+        .map { command -> command.split(" ").mapNotNull { it.toIntOrNull() } }
+
+    val shipPart1 = Ship(List(stacks) { LinkedList() }).apply {
+        init(shipCargo)
+
+        moves.forEach { (count, from, to) ->
+            moveByOne(count, from - 1, to - 1)
+        }
+    }
+    println(shipPart1.peek())
+
+    val shipPart2 = Ship(List(stacks) { LinkedList() }).apply {
+        init(shipCargo)
+
+        moves.forEach { (count, from, to) ->
+            moveMany(count, from - 1, to - 1)
+        }
+    }
+    println(shipPart2.peek())
+}
+
+data class Ship(
+    val stacks: List<Deque<Char>>
+) {
+    fun init(cargo: List<List<Char>>) {
+        cargo.forEach { crates ->
+            repeat(stacks.size) {
+                val crate = crates[it]
+                if (crate != ' ') {
+                    push(it, crate)
+                }
+            }
+        }
+    }
+
+    fun push(to: Int, crate: Char) {
+        stacks[to].push(crate)
+    }
+
+    fun moveByOne(count: Int, from: Int, to: Int) {
+        repeat(count) {
+            val crate = stacks[from].pop()
+            stacks[to].push(crate)
+        }
+    }
+
+    fun moveMany(count: Int, from: Int, to: Int) {
+        val temporaryStack = LinkedList<Char>()
+        repeat(count) {
+            val crate = stacks[from].pop()
+            temporaryStack.push(crate)
+        }
+        repeat(count) {
+            val crate = temporaryStack.pop()
+            stacks[to].push(crate)
+        }
+    }
+
+    fun peek(): String =
+        stacks.map { it.peek() }.joinToString("")
+}
+
